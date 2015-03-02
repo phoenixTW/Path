b89e77e3740cb0bb76f345b4721e3caf9839a576
Kaustav Chakraborty <kaustav.ron@gmail.com>
2015-03-02 17:53:03 +0530

    v2.0b100 | main is done and working

--- Program.java
+++ Program.java
@@ -1,5 +1,12 @@
 class Program {
 	public static void main(String[] args) {
-		
+		String source = args[0];
+		String destination = args[1];
+		Path path = new Path();
+
+		path.insertPath("Bangalore", "Singapore");
+		path.insertPath("Bangalore", "Tokyo");
+
+		System.out.println(path.checkPath(source, destination));
 	}
 }
