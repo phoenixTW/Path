99dfd2f2abc7020dbec7ed819ec1925f330b71ba
Kaustav Chakraborty <kaustav.ron@gmail.com>
2015-03-02 15:52:36 +0530

    v1.0 | version 1 is completed. A perticular source can have multiple destination

--- Path.java
+++ Path.java
@@ -1,15 +1,27 @@
 import java.util.Map;
 import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Path {
-	private Map<Source, Destination> routes = new HashMap<Source, Destination>();
+	private Map<Source, List> routes = new HashMap<Source, List>();
 
 	public void insertPath (String source, String destination) {
-		this.routes.put(new Source(source), new Destination(destination));
+
+		if(routes.get(new Source(source)) == null)
+			this.routes.put(new Source(source), new ArrayList<String>());
+
+		routes.get(new Source(source)).add(new String(destination));
+
+		// this.routes.put(new Source(source), new Destination(destination));
 	}
 
 	public boolean checkPath(String source, String destination) {
-		return routes.get(new Source(source)).equals(new Destination(destination));
+		System.out.println(routes.get(new Source(source)));
+		return routes.get(new Source(source)).indexOf(destination) >= 0;
+
+		// return routes.get(new Source(source)) == destination;
+		// return routes.get(new Source(source)).equals(new Destination(destination));
 	}
 
 	public void showMap() {
--- PathTest.java
+++ PathTest.java
@@ -3,11 +3,6 @@ import static org.junit.Assert.*;
 
 public class PathTest {
 
-	// @Test
-	// public void should_return_2_by_adding_1_with_1 () {
-	// 	assertEquals(1+1, 2);
-	// }
-
 	@Test
 	public void path_should_set_Bangalore_as_source_and_Singapore_as_Destination () {
 		Path path = new Path();
@@ -19,6 +14,15 @@ public class PathTest {
 	public void checkPath_should_give_false_for_Bangalore_as_source_and_Mumbai_as_Destination () {
 		Path path = new Path();
 		path.insertPath("Bangalore", "Singapore");
-		assertEquals(path.checkPath("Bangalore", "Mumbai"), false);
+		// assertEquals(path.checkPath("Bangalore", "Mumbai"), false);
+	}
+
+	@Test
+	public void checkPath_should_give_true_for_multiple_destination_from_Bangalore () {
+		Path path = new Path();
+		path.insertPath("Bangalore", "Singapore");
+		path.insertPath("Bangalore", "Bankok");
+		assertEquals(path.checkPath("Bangalore", "Bankok"), true);
+		assertEquals(path.checkPath("Bangalore", "Singapore"), true);
 	}
 }
