ea693552c26b83af603babf21a869e72739d9f6b
Kaustav Chakraborty <kaustav.ron@gmail.com>
2015-03-02 16:38:17 +0530

    v2.0 | V2.0 is done and tested.

--- Path.java
+++ Path.java
@@ -7,11 +7,16 @@ public class Path {
 	private Map<Source, List> routes = new HashMap<Source, List>();
 
 	public void insertPath (String source, String destination) {
+		Path paths = new Path();
 
 		if(routes.get(new Source(source)) == null)
 			this.routes.put(new Source(source), new ArrayList<String>());
 
+		// System.out.println(">>>>>>>>>>>>" + routes.get(new Destination(destination)) + " - " + destination);
+		if(routes.get(new Source(destination)) == null) this.routes.put(new Source(destination), new ArrayList<String>());
+
 		routes.get(new Source(source)).add(new String(destination));
+		routes.get(new Source(destination)).add(new String(source));
 
 		// this.routes.put(new Source(source), new Destination(destination));
 	}
--- PathTest.java
+++ PathTest.java
@@ -40,4 +40,28 @@ public class PathTest {
 		assertEquals(path.checkPath("Kolkata", "Hyderabad"), true);
 		assertEquals(path.checkPath("Bankok", "Hyderabad"), false);
 	}
+
+	@Test
+	public void checkPath_should_give_true_for_Bangalore_to_Chennai_and_viceversa_for_a_single_call () {
+		Path path = new Path();
+		path.insertPath("Bangalore", "Chennai");
+		assertEquals(path.checkPath("Bangalore", "Chennai"), true);
+		assertEquals(path.checkPath("Chennai", "Bangalore"), true);
+	}
+
+	@Test
+	public void checkPath_should_give_true_for_multiple_destination_from_Different_location_for_single_insertion () {
+		Path path = new Path();
+		path.insertPath("Bangalore", "Singapore");
+		path.insertPath("Bangalore", "Bankok");
+		path.insertPath("Hyderabad", "Kolkata");
+
+		assertEquals(path.checkPath("Bangalore", "Bankok"), true);
+		assertEquals(path.checkPath("Bangalore", "Singapore"), true);
+		assertEquals(path.checkPath("Bankok", "Bangalore"), true);
+		assertEquals(path.checkPath("Singapore", "Bangalore"), true);
+		assertEquals(path.checkPath("Hyderabad", "Kolkata"), true);
+		assertEquals(path.checkPath("Kolkata", "Hyderabad"), true);
+		assertEquals(path.checkPath("Bankok", "Hyderabad"), false);
+	}
 }
