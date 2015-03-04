947e77b6eb0c4f2428adc223ebc529267a381af7
Kaustav Chakraborty <kaustav.ron@gmail.com>
2015-03-04 17:13:49 +0530

    v2.0 | v2.0 isCityPresent is tested for edge Condition

--- LibTest.java
+++ LibTest.java
@@ -4,9 +4,17 @@ import static org.junit.Assert.*;
 public class LibTest {
 
 	@Test
-	public void isCityPresent_should_return_false_for_an_invalid_city_in_routeMap (){
+	public void isCityPresent_should_return_true_for_an_invalid_city_in_routeMap (){
 		RouteMap map = new RouteMap();
 		map.insertPath("Bangalore", "Chennai");
 		assertEquals(true, map.isCityPresent(new City("Bangalore")));
+		assertEquals(true, map.isCityPresent(new City("Chennai")));
+	}
+
+	@Test
+	public void isCityPresent_should_return_false_for_an_invalid_city_in_routeMap (){
+		RouteMap map = new RouteMap();
+		map.insertPath("Bangalore", "Chennai");
+		assertEquals(false, map.isCityPresent(new City("Hyderabad")));
 	}
 }
